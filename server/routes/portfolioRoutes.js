33b9e17086690a57064da0536b35386f91b4ef1b
Roshan4546 <roshanpatra275@gmail.com>
2026-02-25 12:45:17 +0530

    restore private detail route for fetching portfolio by ID with authentication

--- server/routes/portfolioRoutes.js
+++ server/routes/portfolioRoutes.js
@@ -25,10 +25,8 @@ router.get('/my-portfolio', authMiddleware, getUsersPortfolio);
 // router.get("/:slug", getPortfolioBySlug);
 // router.get('/:id', authMiddleware, getSinglePortfolio);
 
-// PRIVATE/DETAIL route - Auth required, uses ID
-router.get('/getThrough/i/:id', authMiddleware, getSinglePortfolio);
-// PUBLIC route - No auth, uses Slug
 router.get("/getThrough/s/:slug", getPortfolioBySlug);
+router.get('/getThrough/i/:id', authMiddleware, getSinglePortfolio);
 
 
 
